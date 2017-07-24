ec1952e9c54a4bbaf0a65560b6589b89660ecf53
saul <slbrtg@gmail.com>
2017-07-24 16:44:20 -0700

    add html and css to layout.vtl

---
+++ src/main/java/App.java
@@ -0,0 +1,21 @@
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+import java.util.Map;
+import java.util.HashMap;
+
+public class App {
+
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new VelocityTemplateEngine().render(
+        new ModelAndView(model, layout)
+      );
+    });
+  }
+}
