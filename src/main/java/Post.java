097b474c61ae8a3b665d105b393ea5b3803d1343
saul <slbrtg@gmail.com>
2017-07-24 12:03:50 -0700

    add getter and setter methods as well as save method to Post.java

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,8 +1,64 @@
+import org.sql2o.*;
+
 public class Post {
   private int id;
-  private int votes;
-  private int subId;
   private String type;
   private String title;
   private String content;
+  private String subName;
+  private int votes;
+
+  //CONSTRUCTOR
+  public Post(String type, String title, String content, String subName){
+    this.type = type;
+    this.title = title;
+    this.content = content;
+    this.subName = subName;
+    this.votes = 0;
+  }
+
+  //GET/SET METHODS
+  public int getId(){
+    return id;
+  }
+
+  public String getType(){
+    return type;
+  }
+
+  public String getTitle(){
+    return title;
+  }
+
+  public String getContent(){
+    return content;
+  }
+
+  public String getSubName(){
+    return subName;
+  }
+
+  public int getVotes(){
+    return votes;
+  }
+
+  public void upvote(){
+    votes += 1;
+  }
+
+  //DATABASE METHODS
+  public void save(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "INSERT INTO posts(title, type, content, votes, subname) VALUES (:title, :type, :content, :votes, :subname);";
+
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("title", this.title)
+        .addParameter("type", this.type)
+        .addParameter("content", this.content)
+        .addParameter("votes", this.votes)
+        .addParameter("subname", this.subName)
+        .executeUpdate()
+      .getKey();
+    }
+  }
 }
--- src/test/java/PostTest.java
+++ src/test/java/PostTest.java
@@ -4,6 +4,19 @@ import static org.junit.Assert.*;
 
 public class PostTest {
 
-  // @Rule
-  // public DatabaseRule database = new DatabaseRule();
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void post_instanceofPost_true(){
+    Post testPost = new Post("red", "red", "","");
+    assertTrue(testPost instanceof Post);
+  }
+
+  @Test
+  public void save_assignsIdToPost_true(){
+    Post testPost = new Post("red", "red", "","");
+    testPost.save();
+    assertTrue(testPost.getId() > 0);
+  }
 }
